5106514ba5899ab40fdda699b57369549b8ee513
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-19 12:13:20 +0900

    Update User.js

--- src/User/User.js
+++ src/User/User.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const user = () => {
+const user = (props) => {
 
   return (
     <div>
-      
+      <p>Hi, I'm {props.name}</p>
+      <input onChange={props.click} value={props.name} />
     </div>
   )
 }
